25d133b662c064fb9a77751c00921e964aa34417
Wolf <andresistuk@gmail.com>
2024-03-13 16:44:26 +0200

    favicon+normalize+structure

---
+++ src/js/scroll.js
@@ -0,0 +1,12 @@
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
