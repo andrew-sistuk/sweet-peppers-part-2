68f0b73e02875fbdf085e7258dc5027a486b1b5d
Wolf <andresistuk@gmail.com>
2024-03-13 17:58:59 +0200

    main fonts and style

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,12 +1,12 @@
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
 for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
+  smoothLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute('href');
 
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
     });
-};
+  });
+}
