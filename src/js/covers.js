f50e333436e752ead13205aae18718431e66d0b3
ArtemSv07 <135955009+ArtemSv07@users.noreply.github.com>
2024-03-14 15:55:24 +0200

    covers-section

---
+++ src/js/covers.js
@@ -0,0 +1,28 @@
+const elements = document.querySelectorAll('.animations');
+const covers = document.querySelector('.content');
+
+function checkIfCoversInView() {
+  const bounding = covers.getBoundingClientRect();
+  const coversInView =
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <=
+      (window.innerWidth || document.documentElement.clientWidth);
+
+  if (coversInView) {
+    if (window.innerWidth < 1440) {
+      elements.forEach(element => {
+        element.classList.add('animations-mobile-tablet');
+      });
+    }
+  } else {
+    elements.forEach(element => {
+      element.classList.remove('animations-mobile-tablet');
+    });
+  }
+}
+
+window.addEventListener('scroll', checkIfCoversInView);
+window.addEventListener('resize', checkIfCoversInView);
