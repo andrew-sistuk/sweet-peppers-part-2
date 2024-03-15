3f6b7bb9eeef9f2099ec0b6cf8094baa9851da4b
Nataliia1113 <orlova.n1113@gmail.com>
2024-03-15 19:23:13 +0200

    faq-tablet

--- src/js/faq.js
+++ src/js/faq.js
@@ -4,24 +4,38 @@ import 'accordion-js/dist/accordion.min.css';
 const ac = document.querySelectorAll('.ac');
 const trigger = document.querySelectorAll('.ac-trigger');
 
+// const acc = new Accordion('.accordion-container', {
+//   duration: 800,
+//   showMultiple: true,
+//   // ariaEnabled: true,
+//   collapse: true,
+//   beforeOpen: currElement => {
+ 
+//   },
+//   onOpen: function (currentElement) {
+//     trigger.forEach(element => {
+//       element.classList.add('trigger-js')
+//     })
+//     console.log(trigger); 
+//   },
+//   onClose: currElement => {
+//     trigger.forEach(element => {
+//       element.classList.removed('trigger-js')
+//     })
+//   },
+// });
+
 const acc = new Accordion('.accordion-container', {
   duration: 800,
   showMultiple: true,
-  // ariaEnabled: true,
   collapse: true,
   beforeOpen: currElement => {
- 
+    const icon = currElement.querySelector('.button-icon');
+
+    icon.classList.add('button-icon-rotste');
   },
-  onOpen: function (currentElement) {
-    trigger.forEach(element => {
-      element.classList.add('trigger-js')
-    })
-    console.log(trigger); 
-  },
-  onClose: currElement => {
-    // trigger.forEach(element => {
-    //   element.classList.removed('trigger-js')
-    // })
+  beforeClose: currElement => {
+    const icon = currElement.querySelector('.button-icon-rotste');
+    icon.classList.remove('button-icon-rotste');
   },
 });
-
