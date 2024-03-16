b399d34a96cb7e0387261a0e7e8e7358d0adf344
davydenko919 <davydenko919@gmail.com>
2024-03-16 18:37:50 +0200

    сейв

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,3 +1,35 @@
 import Accordion from 'accordion-js';
+import Swiper from 'swiper';
 
-new Accordion('.accordion-container');
+new Accordion('.accordeon', {
+  duration: 400,
+  // showMultiple: true,
+  elementClass: '.accardeon-item',
+  triggerClass: '.accardeon-trigger',
+  panelClass: '.about-accordeon-text-container',
+  onOpen: function (currentElement) {
+    console.log(currentElement);
+  },
+});
+
+new Swiper('.about-me-slider',{
+    //стрілки
+    navigation:{
+        nextEl: '.about-me-slider-button',
+        // prevEl: '.swiper-button-prev'
+    },
+   
+    slideToClickedSlide: true,
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+    },
+    
+    mousewheel:{
+        sensitivity: 1,
+    },
+
+    // slidesPerView: 2,
+
+});
