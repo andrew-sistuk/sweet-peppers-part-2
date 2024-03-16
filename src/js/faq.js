a00e9e8cbf40666543d19f277a02e79c7538ca55
Nataliia1113 <orlova.n1113@gmail.com>
2024-03-16 14:51:06 +0200

    faq-desktop

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,29 +1,9 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-const ac = document.querySelectorAll('.ac');
-const trigger = document.querySelectorAll('.ac-trigger');
 
-// const acc = new Accordion('.accordion-container', {
-//   duration: 800,
-//   showMultiple: true,
-//   // ariaEnabled: true,
-//   collapse: true,
-//   beforeOpen: currElement => {
- 
-//   },
-//   onOpen: function (currentElement) {
-//     trigger.forEach(element => {
-//       element.classList.add('trigger-js')
-//     })
-//     console.log(trigger); 
-//   },
-//   onClose: currElement => {
-//     trigger.forEach(element => {
-//       element.classList.removed('trigger-js')
-//     })
-//   },
-// });
+const trigger = document.querySelectorAll('.ac-trigger');
+const buttonIcon = document.querySelectorAll('.button-icon');
 
 const acc = new Accordion('.accordion-container', {
   duration: 800,
@@ -31,7 +11,6 @@ const acc = new Accordion('.accordion-container', {
   collapse: true,
   beforeOpen: currElement => {
     const icon = currElement.querySelector('.button-icon');
-
     icon.classList.add('button-icon-rotste');
   },
   beforeClose: currElement => {
@@ -39,3 +18,48 @@ const acc = new Accordion('.accordion-container', {
     icon.classList.remove('button-icon-rotste');
   },
 });
+
+trigger.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    if (window.innerWidth >= 1440) {
+      switch (String(index)) {
+        case '0':
+          acc.toggle(3);
+          break;
+
+        case '3':
+          acc.toggle(0);
+          break;
+
+        case '1':
+          acc.toggle(4);
+          break;
+
+        case '4':
+          acc.toggle(1);
+          break;
+
+        case '2':
+          acc.toggle(5);
+          break;
+
+        case '5':
+          acc.toggle(2);
+          break;
+
+        default:
+          break;
+      }
+    }
+  });
+});
+
+window.addEventListener('resize', checkIfCoversInView);
+function checkIfCoversInView() {
+  buttonIcon.forEach(elem => {
+    if (elem.classList.contains('button-icon-rotste')) {
+      elem.classList.remove('button-icon-rotste');
+    }
+  });
+  acc.closeAll();
+}
