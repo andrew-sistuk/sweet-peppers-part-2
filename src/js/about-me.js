dbe37f48e5d0628b4bcd3217bdaf6f4cc4d7f98f
davydenko919 <davydenko919@gmail.com>
2024-03-17 14:58:26 +0200

    наче працює

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,57 +1,73 @@
 import Accordion from 'accordion-js';
 import Swiper from 'swiper';
-// import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 
-new Accordion('.accordeon', {
-  duration: 400,
-  // showMultiple: true,
-  elementClass: '.accardeon-item',
-  triggerClass: '.accardeon-trigger',
-  panelClass: '.about-accordeon-text-container',
-  onOpen: function (currentElement) {
-    console.log(currentElement);
+
+const buttonIconAbout = document.querySelectorAll('.about-button-icon');
+
+new Accordion('.accordion-container', {
+  duration: 800,
+  showMultiple: true,
+  collapse: true,
+  openOnInit:[0],
+  beforeOpen: currElement => {
+    const icon = currElement.querySelector('.about-button-icon');
+    icon.classList.add('about-button-icon-rotate');
+  },
+  beforeClose: currElement => {
+    const icon = currElement.querySelector('.about-button-icon');
+    icon.classList.remove('about-button-icon-rotate');
   },
 });
 
-new Swiper('.about-me-slider',{
-    //стрілки
-    navigation:{
-        // nextEl: '.swiper-button-next',
-        nextEl: '.next-about-me-btn',
-        // prevEl: '.swiper-button-prev'
-    },
-   
-    slideToClickedSlide: true,
-
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-    },
-    
-    mousewheel:{
-        sensitivity: 1,
-    },
-
-    // slidesPerView: 2,
-
-    //infinity scroll
-    loop: true,
-
-    // кількість прокручуємих слайдів, за допомогою js знайти який у користувача екран і виставити значення
-    // loopedslides: 2,
-    breakpoints:{
-        320: {
-            // loopedslides: 2,
-            slidesPerView: 2,
-        },
-        768: {
-            // loopedslides: 3,
-            slidesPerView: 3,
-        },
-        1440: {
-            // loopedslides: 6,
-            slidesPerView: 6,
-        }
+window.addEventListener('resize', checkIfCoversInViewAbout);
+function checkIfCoversInViewAbout() {
+    buttonIconAbout.forEach(elem => {
+    if (elem.classList.contains('about-button-icon-rotate')) {
+      elem.classList.remove('about-button-icon-rotate');
     }
+  });
+  acc.closeAll();
+}
 
+
+new Swiper('.about-me-slider', {
+  //стрілки
+  navigation: {
+    // nextEl: '.swiper-button-next',
+    nextEl: '.next-about-me-btn',
+    // prevEl: '.swiper-button-prev'
+  },
+
+  slideToClickedSlide: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+  },
+
+  // slidesPerView: 2,
+
+  //infinity scroll
+  loop: true,
+
+  // кількість прокручуємих слайдів, за допомогою js знайти який у користувача екран і виставити значення
+  // loopedslides: 2,
+  breakpoints: {
+    320: {
+      // loopedslides: 2,
+      slidesPerView: 2,
+    },
+    768: {
+      // loopedslides: 3,
+      slidesPerView: 3,
+    },
+    1440: {
+      // loopedslides: 6,
+      slidesPerView: 6,
+    },
+  },
 });
