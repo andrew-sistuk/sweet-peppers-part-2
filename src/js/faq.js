21a201d8c92f08ad233b4f128374478513ea86bd
MykhailoVobolis <mvobolis@gmail.com>
2024-03-18 19:57:36 +0200

    closing accordion on scroll

--- src/js/faq.js
+++ src/js/faq.js
@@ -4,3 +4,12 @@ import 'accordion-js/dist/accordion.min.css';
 const acc = new Accordion('.accordion-container', {
   duration: 700,
 });
+
+//закриття accordion при виході секції за межі viewport
+const faqSection = document.querySelector('.faq');
+
+faqSection.addEventListener('mouseleave', scrollAction);
+
+function scrollAction(event) {
+  acc.closeAll();
+}
